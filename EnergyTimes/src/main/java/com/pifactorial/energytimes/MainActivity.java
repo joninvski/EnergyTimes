b73a78ae2ddfad2866c780fbed17f9250d3b229b
Joao Trindade <trindade.joao@gmail.com>
2014-03-09 23:02:47 +0000

    Icon update

--- EnergyTimes/src/main/java/com/pifactorial/energytimes/MainActivity.java
+++ EnergyTimes/src/main/java/com/pifactorial/energytimes/MainActivity.java
@@ -20,34 +20,34 @@ import com.pifactorial.energytimes.domain.Schedule;
  * The entry point to the BasicNotification sample.
  */
 public class MainActivity extends Activity {
-	/**
-	 * A numeric value that identifies the notification that we'll be sending.
-	 * This value needs to be unique within this app, but it doesn't need to be
-	 * unique system-wide.
-	 */
-	public static final int NOTIFICATION_ID = 1;
-	public static final int REFRESH_TIME_IN_MINUTES = 1;
+    /**
+     * A numeric value that identifies the notification that we'll be sending.
+     * This value needs to be unique within this app, but it doesn't need to be
+     * unique system-wide.
+     */
+    public static final int NOTIFICATION_ID = 1;
+    public static final int REFRESH_TIME_IN_MINUTES = 1;
 
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_layout);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
 
-		Timer timer = new Timer();
-		TimerTask hourlyTask = new TimerTask() {
-			@Override
-			public void run() {
-				Log.d(Constants.LOG, "Timer called schedule ");
-				runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						setCurrentTime();
-					}
-				});
-			}
-		};
+        Timer timer = new Timer();
+        TimerTask hourlyTask = new TimerTask() {
+            @Override
+            public void run() {
+                Log.d(Constants.LOG, "Timer called schedule ");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setCurrentTime();
+                    }
+                });
+            }
+        };
 
-		// schedule the task to run starting now and then every hour...
-		timer.schedule(hourlyTask, 0l, 1000 * 60 * REFRESH_TIME_IN_MINUTES);
+        // schedule the task to run starting now and then every hour...
+        timer.schedule(hourlyTask, 0l, 1000 * 60 * REFRESH_TIME_IN_MINUTES);
 
         Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
 
@@ -57,48 +57,47 @@ public class MainActivity extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-	}
+    }
 
     @Override
-	public void onResume() {
+    public void onResume() {
         super.onResume();
     }
 
-	public void setCurrentTime() {
-		TextView tvPlan = (TextView) findViewById(R.id.plan);
-		TextView tvStart = (TextView) findViewById(R.id.start);
-		TextView tvEnd = (TextView) findViewById(R.id.end);
+    public void setCurrentTime() {
+        TextView tvPlan = (TextView) findViewById(R.id.plan);
+        TextView tvStart = (TextView) findViewById(R.id.start);
+        TextView tvEnd = (TextView) findViewById(R.id.end);
 
-		Time now = new Time();
-		now.setToNow();
-		tvCurrentPeriod.setText("");
+        Time now = new Time();
+        now.setToNow();
 
-		Populate state = new Populate();
+        Populate state = new Populate();
 
-		try {
-			Schedule s = state.edp.checkCurrentSchedule(now,
-					"BTN Ciclo Semanal");
-			tvPlan.setText(String.format("%s", s.getPrice().getPricePlan()));
-			int startHour = s.getHours().getStartHour();
-			int startMinute = s.getHours().getStartMinute();
-			int endHour = s.getHours().getEndHour();
-			int endMinute = s.getHours().getEndMinute();
+        try {
+            Schedule s = state.edp.checkCurrentSchedule(now,
+                    "BTN Ciclo Semanal");
+            tvPlan.setText(String.format("%s", s.getPrice().getPricePlan()));
+            int startHour = s.getHours().getStartHour();
+            int startMinute = s.getHours().getStartMinute();
+            int endHour = s.getHours().getEndHour();
+            int endMinute = s.getHours().getEndMinute();
 
-			tvStart.setText(String.format("%02dh%02dm", startHour, startMinute));
-			tvEnd.setText(String.format("%02dh%02dm", endHour, endMinute));
+            tvStart.setText(String.format("%02dh%02dm", startHour, startMinute));
+            tvEnd.setText(String.format("%02dh%02dm", endHour, endMinute));
 
-			Log.d(Constants.LOG, "Found schedule " + s.toString());
-		} catch (DayWithoutPlanException e) {
-			Log.e(Constants.LOG, e.getMessage());
+            Log.d(Constants.LOG, "Found schedule " + s.toString());
+        } catch (DayWithoutPlanException e) {
+            Log.e(Constants.LOG, e.getMessage());
             warnUser();
-		} catch (PlanNotFoundException e) {
-			Log.e(Constants.LOG, e.getMessage());
+        } catch (PlanNotFoundException e) {
+            Log.e(Constants.LOG, e.getMessage());
             warnUser();
-		}
-	}
+        }
+    }
 
     private void warnUser(){
-		TextView tvCurrentPeriod = (TextView) findViewById(R.id.tvCurrentPeriod);
-        tvCurrentPeriod.setText("Exception");
+        TextView tvPlan = (TextView) findViewById(R.id.plan);
+        tvPlan.setText("Exception");
     }
 }
