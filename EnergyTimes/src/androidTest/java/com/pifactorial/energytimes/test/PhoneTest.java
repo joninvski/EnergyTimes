fb6503a293bd155353fa213f0e70b9d496de43e6
Joao Trindade <trindade.joao@gmail.com>
2014-04-14 15:45:57 +0100

    Added small robotium test

---
+++ EnergyTimes/src/androidTest/java/com/pifactorial/energytimes/test/PhoneTest.java
@@ -0,0 +1,32 @@
+package com.pifactorial.energytimes.test;
+
+import com.robotium.solo.*;
+import android.test.ActivityInstrumentationTestCase2;
+import com.pifactorial.energytimes.MainActivity;
+
+public class PhoneTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	private Solo solo;
+
+	public PhoneTest() {
+		super(MainActivity.class);
+	}
+
+	public void setUp() throws Exception {
+		solo = new Solo(getInstrumentation());
+		getActivity();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
+	public void testRun() {
+
+		int timeout = 5;
+
+		// Wait for activity: 'course.labs.fragmentslab.MainActivity'
+		assertTrue("MainActivity not found", solo.waitForActivity(
+				com.pifactorial.energytimes.MainActivity.class, 2000));
+	}
+}
